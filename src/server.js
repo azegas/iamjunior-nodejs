4fe5b66ff2165ad45a3ca151500ef74368697861
azegas <azegaspa@gmail.com>
2024-10-31 05:41:07 +0200

    hello world express

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
-console.log("Kazkas veikia");
-console.log("Kazkas veikia");
-console.log("Kazkas veikia");
-console.log("Kazkas veikia");
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
