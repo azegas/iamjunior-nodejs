174d7f737fe65ddf228155ae6956fc024f801fe5
azegas <azegaspa@gmail.com>
2024-10-31 05:45:53 +0200

    ok now real hello world

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+// viska ka gaunu is uzklausos siuntejo - gausiu per req parametra
+// viska ka siusiu atgal - galiu irasyti i res parametra
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on http://localhost:3000");
 });
