fadcd035129b8fff7711e7e49808bf47c80c0b92
azegas <azegaspa@gmail.com>
2024-11-01 05:14:12 +0200

    modify category post request

--- src/categories/mutations/post-category.js
+++ src/categories/mutations/post-category.js
@@ -1,16 +1,13 @@
 const { categories } = require('../mock-categories');
 
 function postCategory(req, res) {
-    // body, during request is encrypted and compressed, that's why we can not see it
-    console.log(req.body);
-
-    // logika, pridedam nauja kategorija i masyva
     categories.push({
         id: categories.length + 1,
-        name: req.body.name
+        name: req.body.name,
+        color: req.body.color,
+        url: req.body.url
     });
     
-    // atsakymas klientui
     res.json({
         success: true,
         message: 'Category created successfully',
