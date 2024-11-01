9eede3221c2872407d060d3fe4754b9fc2f8a09b
azegas <azegaspa@gmail.com>
2024-11-01 07:33:49 +0200

    inform if no businesses found

--- src/businesses/queries/get-businesses-all.js
+++ src/businesses/queries/get-businesses-all.js
@@ -5,6 +5,9 @@ http://localhost:3000/api/businesses
 */
 
 function getBusinesses(req, res) {
+  if (businesses.length === 0) {
+    return res.status(404).json({ success: false, message: 'No businesses found.' });
+  }
   res.json(businesses);
 }
 
