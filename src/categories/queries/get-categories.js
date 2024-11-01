8b425d1900c0322976e569a5067de0650754ec6d
azegas <azegaspa@gmail.com>
2024-11-01 07:32:29 +0200

    inform if no categories

--- src/categories/queries/get-categories.js
+++ src/categories/queries/get-categories.js
@@ -5,6 +5,9 @@ http://localhost:3000/api/categories
 */
 
 function getCategories(req, res) {
+  if (categories.length === 0) {
+    return res.status(404).json({ success: false, message: 'No categories found.' });
+  }
   res.json(categories);
 }
 
